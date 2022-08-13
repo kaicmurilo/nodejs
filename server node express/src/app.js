e08e46faf20a6df0b9bf63867faf75b5eb58969b
Kaic <kaicmurilo@live.com>
2022-08-13 16:34:00 -0400

    estudando server com express

---
+++ server node express/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+const livros = [
+    {
+        id: 1, "titulo": "Senhor dos Aneis"
+    },
+    { id: 2, "titulo": "Hobiit" }
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Node');
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+export default app
--- server node simples/server.js
+++ server node simples/server.js
@@ -1,9 +1,18 @@
 const http = require("http")
 const port = 3000;
 
+
+const rotas = {
+    '/': 'Node',
+    '/livros': 'Pagina de Livros',
+    '/autores': 'Listagem de autores',
+    '/editora' : 'Editoras',
+    '/sobre': 'Info sobre projeto'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Servidor Node Manual');
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, () => {
