a7ce48ea10d30964f307f11149427151aabfd6b2
Kaic <kaicmurilo@live.com>
2022-08-20 18:52:59 -0400

    att de codigo

--- orm node sql/api/index.js
+++ orm node sql/api/index.js
@@ -5,16 +5,13 @@ const app = express()
 
 app.use(bodyParser.json())
 
-
 const port = 3000
 
-app.get('/teste', (req, res) => {
-    res
-        .status(200)
-        .send({ mensagem: 'boas-vindas API' })
-})
+app.get('/teste', (req, res) => res
+  .status(200)
+  .send({ mensagem: 'boas-vindas à API' 
+}))
 
-app.listen(port, () => console.log(`servidor rodando na porta ${port}`))
-
+app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
 
 module.exports = app
