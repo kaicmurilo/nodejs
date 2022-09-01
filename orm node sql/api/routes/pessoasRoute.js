e80855672928d5077dfbd8d0a4c3bcf9daeb37cf
Kaic Nunes <kaic.nunes@MASCARENHAS.CORP>
2022-08-31 20:18:00 -0400

    metodo git one

--- orm node sql/api/routes/pessoasRoute.js
+++ orm node sql/api/routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessa)
 
 module.exports = router
